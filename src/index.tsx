c22c67900e799e6c9adf66592a7e1ecce12a4098
Elvinho <elvison.victor@liferay.com>
2025-05-19 11:35:44 -0300

    add gitignore e ajustes

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Portfolio from "./Portfolio";
+import App from "./Portfolio";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
-    <Portfolio />
+    <App />
   </React.StrictMode>
 );
